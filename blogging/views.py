e6d2c8be8e59758bd4acf8bd98d5922d673f72a3
kathleen101 <81065967+kathleen101@users.noreply.github.com>
2022-09-14 18:10:56 -0700

    Removed unnecessary line.

--- blogging/views.py
+++ blogging/views.py
@@ -6,7 +6,6 @@ from django.views.generic.detail import DetailView
 class PostListView(ListView):
     model = Post
     template_name = 'blogging/list.html'
-    context_object_name = 'posts'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
