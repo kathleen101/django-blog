e8130d402176eb590e28fe7498a181e9b9cf7a4b
kathleen101 <81065967+kathleen101@users.noreply.github.com>
2022-09-18 16:46:03 -0700

    Updated travis file to switch branch.

--- blogging/views.py
+++ blogging/views.py
@@ -4,16 +4,10 @@ from django.views.generic.detail import DetailView
 
 
 class PostListView(ListView):
-    model = Post
-    template_name = 'blogging/list.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['posts'] = Post.objects.all().exclude(published_date__exact=None).order_by('-published_date')
-        return context
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = "blogging/list.html"
 
 
 class PostDetailView(DetailView):
-    model = Post
     template_name = 'blogging/detail.html'
     queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
