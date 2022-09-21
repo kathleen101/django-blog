33caa8dcf3e7866ddfba5d625107e84b32ec30a1
kathleen101 <81065967+kathleen101@users.noreply.github.com>
2022-09-20 19:38:11 -0700

    Updated blogging/views to run.

--- blogging/views.py
+++ blogging/views.py
@@ -4,8 +4,13 @@ from django.views.generic.detail import DetailView
 
 
 class PostListView(ListView):
-    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
-    template_name = "blogging/list.html"
+    model = Post
+    template_name = 'blogging/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.all().exclude(published_date__exact=None).order_by('-published_date')
+        return context
 
 
 class PostDetailView(DetailView):
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,4 @@
 """mysite URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -17,10 +16,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 
+
 urlpatterns = [
+    path('', include('blogging.urls')),
     path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
-    path('', include('blogging.urls')),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
