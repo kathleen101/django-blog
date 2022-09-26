4364cb08dbb91e2c52963eaba926dfa8f3f906c2
kathleen101 <81065967+kathleen101@users.noreply.github.com>
2022-09-25 22:13:04 -0700

    Updated to allow login with Github.

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+
+    "allauth",  # new
+    "allauth.account",  # new
+    "allauth.socialaccount",  # new
+    "allauth.socialaccount.providers.github",
+    
     "polling",
     "blogging",
 ]
@@ -125,3 +132,14 @@ USE_TZ = True
 STATIC_URL = "/static/"
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+LOGIN_REDIRECT_URL =  '/'
